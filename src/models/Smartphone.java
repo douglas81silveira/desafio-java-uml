0772794f6ef612ad5107828912b06a43fdcff167
Douglas Silveira <douglas81silveira@gmail.com>
2023-09-02 23:39:14 -0300

    alterada a classe smartphone

--- src/models/Smartphone.java
+++ src/models/Smartphone.java
@@ -9,11 +9,6 @@ public class Smartphone implements ITelefone, IReprodutorMusical, INavegadorInte
     private String marca;
     private String nome;
 
-    public Smartphone() {
-        this.marca = "Smartphone";
-        this.nome = "Genérico";
-    }
-
     public Smartphone(String marca, String nome) {
         this.marca = marca;
         this.nome = nome;
